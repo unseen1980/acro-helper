8067fa7031b4421ccd4fe08d9cf58ff6833bf995
Christos Koutsiaris <christos.koutsiaris@sap.com>
2023-03-18 00:48:22 +0000

    Use different parameter name, string is reserved keyword

--- lib/replacer.ts
+++ lib/replacer.ts
@@ -1,14 +1,18 @@
 /**
 Replaces all occurrences of a string in the HTML content of a given selector with a definition and wraps it in a <dfn> element with an <abbr> title attribute.
 @param {string} selector - The CSS selector of the element to modify.
-@param {string} string - The string to search for and replace.
+@param {string} word - The string to search for and replace.
 @param {string} definition - The definition to use for the <abbr> title attribute.
 @returns {void}
 */
-export const replacer = (selector, string, definition) => {
+export const replacer = (
+  selector: string,
+  word: string,
+  definition: string
+) => {
   let dom = document.querySelector(selector)
   let str = dom.innerHTML //A
-  let esc = `(?!(?:[^<]+>|[^>]+<\\/a>))\\b(${string})\\b` //B
+  let esc = `(?!(?:[^<]+>|[^>]+<\\/a>))\\b(${word})\\b` //B
   let rgx = new RegExp(esc, "gi") //C
   let txt = str.replace(rgx, `<dfn><abbr title="${definition}">$1</abbr></dfn>`) //D
   dom.replaceChildren() //E
@@ -23,7 +27,13 @@ Finds text that matches a pattern within an HTML element and executes a callback
 @param {Function} callback - The function to execute for each match. The function takes two arguments: the matched text node and the match object.
 @returns {void}
 */
-export const findText = (element, pattern, callback) => {
+export const findText = (
+  element: { childNodes: string | any[] },
+  pattern: { exec: (arg0: any) => any },
+  callback: {
+    call: (arg0: Window & typeof globalThis, arg1: any, arg2: any) => void
+  }
+) => {
   for (let childi = element.childNodes.length; childi-- > 0; ) {
     let child = element.childNodes[childi]
     if (child.nodeType == 1) {
